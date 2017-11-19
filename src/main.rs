b6eb5235e1b2bb6d953ab4e855bf64de83fa771c
Chiu-Hsiang Hsu <wdv4758h@gmail.com>
2017-11-19 14:10:59 +0800

    feat: update to conrod 0.56.0 and rewrite the code
    https://github.com/PistonDevelopers/conrod/blob/master/examples/image.rs

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,77 @@
+// modify from conrod "image" example
+
 #[macro_use]
 extern crate clap;              // CLI arguments
-
-#[macro_use] extern crate conrod;
-extern crate find_folder;
-extern crate piston_window;
-
-use clap::App;              // CLI arguments
-use conrod::{Canvas, Colorable, Image, Positionable, Theme, Widget, color};
-use piston_window::{EventLoop, Flip, PistonWindow, Texture, UpdateEvent, WindowSettings};
-
-use std::path::Path;
-use std::sync::Arc;
+#[macro_use]
+extern crate conrod;
+extern crate image;
 
 
-// Conrod is backend agnostic. Here, we define the `piston_window` backend to use for our `Ui`.
-type Backend = (piston_window::G2dTexture<'static>, piston_window::Glyphs);
-type Ui = conrod::Ui<Backend>;
+use clap::App;                  // CLI arguments
+use conrod::{widget, Colorable, Positionable, Sizeable, Widget, color};
+use conrod::backend::glium::glium;
+use conrod::backend::glium::glium::Surface;
 
+
+/// In most of the examples the `glutin` crate is used for providing the window context and
+/// events while the `glium` crate is used for displaying `conrod::render::Primitives` to the
+/// screen.
+///
+/// This `Iterator`-like type simplifies some of the boilerplate involved in setting up a
+/// glutin+glium event loop that works efficiently with conrod.
+pub struct EventLoop {
+    ui_needs_update: bool,
+    last_update: std::time::Instant,
+}
+
+impl EventLoop {
+
+    pub fn new() -> Self {
+        EventLoop {
+            last_update: std::time::Instant::now(),
+            ui_needs_update: true,
+        }
+    }
+
+    /// Produce an iterator yielding all available events.
+    pub fn next(&mut self, events_loop: &mut glium::glutin::EventsLoop) -> Vec<glium::glutin::Event> {
+        // We don't want to loop any faster than 60 FPS, so wait until it has been at least 16ms
+        // since the last yield.
+        let last_update = self.last_update;
+        let sixteen_ms = std::time::Duration::from_millis(16);
+        let duration_since_last_update = std::time::Instant::now().duration_since(last_update);
+        if duration_since_last_update < sixteen_ms {
+            std::thread::sleep(sixteen_ms - duration_since_last_update);
+        }
+
+        // Collect all pending events.
+        let mut events = Vec::new();
+        events_loop.poll_events(|event| events.push(event));
+
+        // If there are no events and the `Ui` does not need updating, wait for the next event.
+        if events.is_empty() && !self.ui_needs_update {
+            events_loop.run_forever(|event| {
+                events.push(event);
+                glium::glutin::ControlFlow::Break
+            });
+        }
+
+        self.ui_needs_update = false;
+        self.last_update = std::time::Instant::now();
+
+        events
+    }
+
+    /// Notifies the event loop that the `Ui` requires another update whether or not there are any
+    /// pending events.
+    ///
+    /// This is primarily used on the occasion that some part of the `Ui` is still animating and
+    /// requires further updates to do so.
+    pub fn needs_update(&mut self) {
+        self.ui_needs_update = true;
+    }
+
+}
 
 fn main() {
 
@@ -24,7 +79,7 @@ fn main() {
     // Parse Arguments
     ////////////////////
 
-    let yml = load_yaml!("arguments.yml");
+    let yml = load_yaml!("cli.yml");
     let arguments = App::from_yaml(yml).get_matches();
     let file = arguments.value_of("file").unwrap();
 
@@ -32,53 +87,101 @@ fn main() {
     // Creating Window
     ////////////////////
 
-    // Change this to OpenGL::V2_1 if not working.
-    let opengl = piston_window::OpenGL::V3_2;
+    const WIDTH: u32 = 800;
+    const HEIGHT: u32 = 600;
 
-    // Construct the window.
-    let mut window: PistonWindow =
-        WindowSettings::new("Image Widget Demonstration", [800, 600])
-            .opengl(opengl).exit_on_esc(true).vsync(true).samples(4).build().unwrap();
+    let mut events_loop = glium::glutin::EventsLoop::new();
+    let window = glium::glutin::WindowBuilder::new()
+        .with_title("Image Widget Demonstration")
+        .with_dimensions(WIDTH, HEIGHT);
+
+    let context = glium::glutin::ContextBuilder::new()
+        .with_vsync(true)
+        .with_multisampling(4);
+    let display = glium::Display::new(window, context, &events_loop).unwrap();
+
 
     ////////////////////
-    // Finding Assets
+    // Creating UI
     ////////////////////
-
-    // Get the path to our `assets` directory (where the fonts are).
-    let assets = find_folder::Search::ParentsThenKids(3, 3).for_folder("assets").unwrap();
 
     // construct our `Ui`.
-    let mut ui = {
-        let font_path = assets.join("fonts/NotoSans/NotoSans-Regular.ttf");
-        let theme = Theme::default();
-        let glyph_cache = piston_window::Glyphs::new(&font_path, window.factory.clone()).unwrap();
-        Ui::new(glyph_cache, theme)
-    };
+    let mut ui = conrod::UiBuilder::new([WIDTH as f64, HEIGHT as f64]).build();
 
-    // The texture to use for the `Image`.
-    let image = {
-        let path = Path::new(file);
-        let factory = &mut window.factory;
-        let settings = piston_window::TextureSettings::new();
-        Arc::new(Texture::from_path(factory, &path, Flip::None, &settings).unwrap())
-    };
+    // A type used for converting `conrod::render::Primitives` into `Command`s that can be used
+    // for drawing to the glium `Surface`.
+    let mut renderer = conrod::backend::glium::Renderer::new(&display).unwrap();
+
 
     ////////////////////
-    // Set Up Window
+    // Image
     ////////////////////
 
-    window.set_ups(60);
+    // The `WidgetId` for our background and `Image` widgets.
+    widget_ids!(struct Ids { background, image });
+    let ids = Ids::new(ui.widget_id_generator());
+
+    // Create our `conrod::image::Map` which describes each of our widget->image mappings.
+    // In our case we only have one image, however the macro may be used to list multiple.
+    let image = load_image(&display, file);
+    let (w, h) = (image.get_width(), image.get_height().unwrap());
+    let mut image_map = conrod::image::Map::new();
+    let image = image_map.insert(image);
 
     // Poll events from the window.
-    while let Some(event) = window.next() {
-        ui.handle_event(event.clone());
-        window.draw_2d(&event, |c, g| ui.draw_if_changed(c, g));
-        event.update(|_| ui.set_widgets(|mut ui| {
-            widget_ids!(CANVAS, IMAGE);
-            Canvas::new().color(color::LIGHT_BLUE).set(CANVAS, &mut ui);
-            Image::from_texture(image.clone())
-                .middle_of(CANVAS)
-                .set(IMAGE, &mut ui);
-        }));
+    let mut event_loop = EventLoop::new();
+    'main: loop {
+
+        // Handle all events.
+        for event in event_loop.next(&mut events_loop) {
+
+            // Use the `winit` backend feature to convert the winit event to a conrod one.
+            if let Some(event) = conrod::backend::winit::convert_event(event.clone(), &display) {
+                ui.handle_event(event);
+            }
+
+            match event {
+                glium::glutin::Event::WindowEvent { event, .. } => match event {
+                    // Break from the loop upon `Escape`.
+                    glium::glutin::WindowEvent::Closed |
+                        glium::glutin::WindowEvent::KeyboardInput {
+                            input: glium::glutin::KeyboardInput {
+                                virtual_keycode: Some(glium::glutin::VirtualKeyCode::Escape),
+                                ..
+                            },
+                            ..
+                        } => break 'main,
+                    _ => (),
+                },
+                _ => (),
+            }
+        }
+
+        // Instantiate the widgets.
+        {
+            let ui = &mut ui.set_widgets();
+            // Draw a light blue background.
+            widget::Canvas::new().color(color::BLACK).set(ids.background, ui);
+            // Instantiate the `Image` at its full size in the middle of the window.
+            widget::Image::new(image).w_h(w as f64, h as f64).middle().set(ids.image, ui);
+        }
+
+        // Render the `Ui` and then display it on the screen.
+        if let Some(primitives) = ui.draw_if_changed() {
+            renderer.fill(&display, primitives, &image_map);
+            let mut target = display.draw();
+            target.clear_color(0.0, 0.0, 0.0, 1.0);
+            renderer.draw(&display, &mut target, &image_map).unwrap();
+            target.finish().unwrap();
+        }
     }
+}
+
+
+fn load_image(display: &glium::Display, filepath: &str) -> glium::texture::Texture2d {
+    let rgba_image = image::open(&std::path::Path::new(&filepath)).unwrap().to_rgba();
+    let image_dimensions = rgba_image.dimensions();
+    let raw_image = glium::texture::RawImage2d::from_raw_rgba_reversed(&rgba_image.into_raw(), image_dimensions);
+    let texture = glium::texture::Texture2d::new(display, raw_image).unwrap();
+    texture
 }
